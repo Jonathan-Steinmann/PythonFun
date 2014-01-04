8347f9ec6ed59cadc3c63243fc9e94e85a42e823
Jonathan Steinmann <info@cmiwebstudio.com>
2014-01-04 02:47:59 -0500

    multi-python version support and additional fun

--- countdown.py
+++ countdown.py
@@ -1,94 +1,108 @@
-import time
-import os
+class Countdown:
 
-S120 = 120
-S1500 = 1500
-S2000 = 2000
-S2500 = 2500
-FAIL = '\033[91m'
-OKGREEN = '\033[92m'
-ENDC = '\033[0m'
+	import time
+	import os
 
-Art1 = [
-	" ", "I MUST USE WINDOWS 8 AS INSTRUCTED.           ",  "                           \                  ", 
-	"                                .....         ", "                               C C  /         ", "                              /<   /          ", 
-	"               ___ __________/_#__=o          ", "              /(- /(\_\________   \           ", "              \ ) \ )_      \o     \          ", 
-	"              /|\ /|\       |'     |          ", "                            |     _|          ", "                            /o   __\          ", 
-	"                           / '     |          ", "                          / /      |          ", "                         /_/\______|          ", 
-	"                        (   _(    <           ", "                         \    \    \          ", "                          \    \    |         ", 
-	"                           \____\____\        ", "                           ____\_\__\_\       ", "                         /`   /`     o\       ", 
-	"                         |___ |_______|       ", " "
-]
+	S120 = 120
+	S1500 = 1500
+	S2000 = 2000
+	S2500 = 2500
+	FAIL = '\033[91m'
+	OKGREEN = '\033[92m'
+	ENDC = '\033[0m'
+	WIN8 = "I MUST USE WINDOWS 8 AS INSTRUCTED.           "
+	ALERT = "CODE RED... MAJOR FAIL DETECTION ALERT... MUST ABORT WINDOWS 8..."
+	START = "ANTI-WINDOWS SEQUENCE STARTING..."
 
-Art2 = [
-	" ", " ", " _   _| |__  _   _ _ __ | |_ _   _ ", "| | | | '_ \| | | | '_ \| __| | | |", "| |_| | |_) | |_| | | | | |_| |_| |", 
-	" \__,_|_.__/ \__,_|_| |_|\__|\__,_|", " ", "               | |       ", "  _ __ __   ___| | _____ ", "| '__/ _ \ / __| |/ / __|", 
-	"| | | (_) | (__|   <\__ \.", "|_|  \___/ \___|_|\_\___/", " ", " "
-]
+	Art1 = [
+		" ", WIN8,  "                           \                  ", 
+		"                                .....         ", "                               C C  /         ", "                              /<   /          ", 
+		"               ___ __________/_#__=o          ", "              /(- /(\_\________   \           ", "              \ ) \ )_      \o     \          ", 
+		"              /|\ /|\       |'     |          ", "                            |     _|          ", "                            /o   __\          ", 
+		"                           / '     |          ", "                          / /      |          ", "                         /_/\______|          ", 
+		"                        (   _(    <           ", "                         \    \    \          ", "                          \    \    |         ", 
+		"                           \____\____\        ", "                           ____\_\__\_\       ", "                         /`   /`     o\       ", 
+		"                         |___ |_______|       ", " "
+	]
 
-def checkOS():
-	if os.name == 'nt':
-		import winsound
-		return True
-	else:
-		return False
+	Art2 = [
+		" _   _| |__  _   _ _ __ | |_ _   _ ", "| | | | '_ \| | | | '_ \| __| | | |", 
+		"| |_| | |_) | |_| | | | | |_| |_| |", " \__,_|_.__/ \__,_|_| |_|\__|\__,_|"
+	]
 
-def napTime(ms=1):
-	time.sleep(ms)
+	Art3 = [
+		" _   _| |__  _   _ _ __ | |_ _   _ ", "| | | | '_ \| | | | '_ \| __| | | |", "| |_| | |_) | |_| | | | | |_| |_| |", 
+		" \__,_|_.__/ \__,_|_| |_|\__|\__,_|", " ", "               | |       ", "  _ __ __   ___| | _____ ", "| '__/ _ \ / __| |/ / __|", 
+		"| | | (_) | (__|   <\__ \.", "|_|  \___/ \___|_|\_\___/", " ", " "
+	]
 
-def soundFactory(freq, dur):
-	if checkOS == True:
-		winsound.Beep(freq, dur)
-	else:
-		print '\a'
+	def checkOS(self):
+		if os.name == 'nt':
+			import winsound
+			return True
+		else:
+			return False
 
-def theMostAnnoyingSoundInTheWorld(n=0):
-	for value in range(n):
-		soundFactory(S1500, S2000); 
+	def napTime(self, ms=1):
+		self.time.sleep(ms)
 
-def theLessAnnoyingSoundSequence(n=0):
-	for value in range(n):
-		soundFactory(S1500, S120)
-		soundFactory(S2000, S120)
-		soundFactory(S2500, S120)
+	def soundFactory(self, freq, dur):
+		if self.checkOS == True:
+			self.winsound.Beep(freq, dur)
+		else:
+			print("\a")
 
-def sanityCountdown(seconds=0):
-	while seconds > 0:
-		napTime(1)
-		print (" " + str(seconds))
-		seconds = seconds-1
-	else:
-		napTime(1)
-		for row1 in Art1:
-			napTime(1)
-			print(row1)
-		print(FAIL + "MAJOR FAIL DETECTION ALERT... MUST STOP WINDOWS 8..." + ENDC)
-		napTime(1)
-		theMostAnnoyingSoundInTheWorld(1)
-		napTime(1)
-		theLessAnnoyingSoundSequence(3)
-		napTime(3)
-		print(OKGREEN + "anti-Windows sequence started..." + ENDC)
-		napTime(3)
-		for x in range(0,20000):
-			for row2 in Art2:
-				print(row2)
-		for row3 in Art2:
-			napTime(1)
-			print(OKGREEN + row3)
-		print(":) \r\n" + ENDC)
+	def theMostAnnoyingSoundInTheWorld(self, n=0):
+		for value in range(n):
+			self.soundFactory(self.S1500, self.S2000); 
+
+	def theLessAnnoyingSoundSequence(self, n=0):
+		for value in range(n):
+			self.soundFactory(self.S1500, self.S120)
+			self.soundFactory(self.S2000, self.S120)
+			self.soundFactory(self.S2500, self.S120)
+
+	def sanityCountdown(self, seconds=0):
+		while seconds > 0:
+			self.napTime(1)
+			print (" " + str(seconds))
+			seconds = seconds-1
+		else:
+			self.napTime(1)
+			for row1 in self.Art1:
+				self.napTime(1)
+				print(row1)
+			print(self.FAIL + self.ALERT + self.ENDC)
+			self.napTime(1)
+			self.theMostAnnoyingSoundInTheWorld(1)
+			self.napTime(1)
+			self.theLessAnnoyingSoundSequence(3)
+			self.napTime(3)
+			print(self.OKGREEN + self.START + self.ENDC)
+			self.napTime(3)
+			for x in range(0,100000):
+				for row2 in self.Art2:
+					print(row2)
+			for y in range(0,50):
+				print("\r\n")
+			for row3 in self.Art3:
+				self.napTime(1)
+				print(self.OKGREEN + row3)
+			print(":) \r\n" + self.ENDC)
 
 
 #run option - no requirements
 if __name__ == '__main__':
-	sanityCountdown()
+	countdown = Countdown()
+	countdown.sanityCountdown()
 
 # run option - require countdown
 # if __name__ == '__main__':
 # 	try:
-#		import sys
+# 		import sys
+# 		countdown = Countdown()
 # 		seconds = (int(sys.argv[1]))
-# 		sanityCountdown(seconds)
+# 		countdown.sanityCountdown(seconds)
 # 	except IndexError:
 # 		print("How to use: countdown.py <number>")
 
