8262c648d90ef8a02b9b58b99c9f51e86f3aae82
Jonathan Steinmann <info@cmiwebstudio.com>
2014-01-04 17:33:48 -0500

    setup basic unit testing file

---
+++ tests/countdown.py
@@ -0,0 +1,15 @@
+import unittest
+
+class testCountdown(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def testExample(self):
+		result = 2+2
+		self.assertEqual(4, result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
