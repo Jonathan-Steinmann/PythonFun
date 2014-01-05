f6ab03bd3104d03617303c5e565a30b597140f5c
Jonathan Steinmann <info@cmiwebstudio.com>
2014-01-04 20:50:17 -0500

    update/add test

--- countdown-test.py
+++ countdown-test.py
@@ -1,9 +1,15 @@
+#requires python 3
 import unittest
+from unittest.mock import patch
+import random
 from countdown import Countdown
 
-class testCountdown(unittest.TestCase):
+class countdownTest(unittest.TestCase):
 
 	def setUp(self):
+		pass
+
+	def tearDown(self):
 		pass
 
 	def testExample(self):
@@ -11,9 +17,18 @@ class testCountdown(unittest.TestCase):
 		self.assertEqual(4, result)
 
 	def testGetArt(self):
+		#action
 		result = Countdown.getArt(Countdown, "art3")
+		#assert
 		self.assertEqual(Countdown.Art3, result)
 
+	def testTheMostAnnoyingSoundInTheWorld(self):
+		#setup
+		sound = Countdown.soundFactory(Countdown, Countdown.S1500, Countdown.S2000)
+		#action
+		result = Countdown.theMostAnnoyingSoundInTheWorld(Countdown, 0)
+		#assert
+		self.assertEqual(sound, result)
 
 
 if __name__ == '__main__':
